864bec86ff31cd1272e48e9f43f3e7010079b30b
Gergely Brautigam <gergely@arangodb.com>
2020-11-02 21:14:57 +0100

    Adding the command

--- cmd/root.go
+++ cmd/root.go
@@ -1,25 +1,42 @@
 package cmd
 
 import (
-	"flag"
+	"github.com/spf13/cobra"
 
+	"github.com/krok-o/krok/pkg/krok/providers/environment"
+	"github.com/krok-o/krok/pkg/krok/providers/livestore"
+	"github.com/krok-o/krok/pkg/krok/providers/plugins"
 	"github.com/krok-o/krok/pkg/server"
 )
 
 var (
-	rootArgs struct {
-		devMode bool
-		debug   bool
-		server  server.Config
+	krokCmd = &cobra.Command{
+		Use:   "krok",
+		Short: "Krok server",
+		Run:   runKrokCmd,
+	}
+	krokArgs struct {
+		devMode     bool
+		debug       bool
+		server      server.Config
+		environment environment.Config
+		store       livestore.Config
+		plugins     plugins.Config
 	}
 )
 
 func init() {
-	flag.BoolVar(&rootArgs.server.AutoTLS, "auto-tls", false, "--auto-tls")
-	flag.BoolVar(&rootArgs.debug, "debug", false, "--debug")
-	flag.StringVar(&rootArgs.server.CacheDir, "cache-dir", "", "--cache-dir /home/user/.server/.cache")
-	flag.StringVar(&rootArgs.server.ServerKeyPath, "server-key-path", "", "--server-key-path /home/user/.server/server.key")
-	flag.StringVar(&rootArgs.server.ServerCrtPath, "server-crt-path", "", "--server-crt-path /home/user/.server/server.crt")
-	flag.StringVar(&rootArgs.server.Port, "port", "9998", "--port 443")
-	flag.Parse()
+	flag := krokCmd.Flags()
+	// Server Configs
+	flag.BoolVar(&krokArgs.server.AutoTLS, "auto-tls", false, "--auto-tls")
+	flag.BoolVar(&krokArgs.debug, "debug", false, "--debug")
+	flag.StringVar(&krokArgs.server.CacheDir, "cache-dir", "", "--cache-dir /home/user/.server/.cache")
+	flag.StringVar(&krokArgs.server.ServerKeyPath, "server-key-path", "", "--server-key-path /home/user/.server/server.key")
+	flag.StringVar(&krokArgs.server.ServerCrtPath, "server-crt-path", "", "--server-crt-path /home/user/.server/server.crt")
+	flag.StringVar(&krokArgs.server.Port, "port", "9998", "--port 443")
+	// Store config
+
+}
+
+func runKrokCmd(cmd *cobra.Command, args []string) {
 }
