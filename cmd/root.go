c8712885b1f9514740698944f65d26b8ffd4f4e1
Gergely Brautigam <gergely@arangodb.com>
2020-11-03 09:14:36 +0100

    work on the server

--- cmd/root.go
+++ cmd/root.go
@@ -1,7 +1,12 @@
 package cmd
 
 import (
+	"context"
+	"os"
+
+	"github.com/rs/zerolog"
 	"github.com/spf13/cobra"
+	"golang.org/x/sync/errgroup"
 
 	"github.com/krok-o/krok/pkg/krok/providers/environment"
 	"github.com/krok-o/krok/pkg/krok/providers/livestore"
@@ -34,9 +39,33 @@ func init() {
 	flag.StringVar(&krokArgs.server.ServerKeyPath, "server-key-path", "", "--server-key-path /home/user/.server/server.key")
 	flag.StringVar(&krokArgs.server.ServerCrtPath, "server-crt-path", "", "--server-crt-path /home/user/.server/server.crt")
 	flag.StringVar(&krokArgs.server.Port, "port", "9998", "--port 443")
-	// Store config
 
+	// Store config
+	flag.StringVar(&krokArgs.store.Database, "krok-db-dbname", "krok", "--krok-db-dbname krok")
+	flag.StringVar(&krokArgs.store.Username, "krok-db-username", "krok", "--krok-db-username krok")
+	flag.StringVar(&krokArgs.store.Password, "krok-db-password", "krok", "--krok-db-password password123")
+	flag.StringVar(&krokArgs.store.Hostname, "krok-db-hostname", "", "--krok-db-hostname krok-db")
+
+	// Plugins
+	flag.StringVar(&krokArgs.plugins.Location, "krok-plugin-location", "/tmp/krok/plugins", "--krok-plugin-location /tmp/krok/plugins")
 }
 
 func runKrokCmd(cmd *cobra.Command, args []string) {
+	ctx := context.Background()
+	out := zerolog.ConsoleWriter{
+		Out: os.Stderr,
+	}
+	log := zerolog.New(out).With().
+		Timestamp().
+		Logger()
+
+	// Create server
+	server := server.NewKrokServer(krokArgs.server, server.Dependencies{})
+
+	// Run service & server
+	g, ctx := errgroup.WithContext(context.Background())
+	g.Go(func() error { return server.Run(ctx) })
+	if err := g.Wait(); err != nil {
+		log.Err(err).Msg("Failed to run")
+	}
 }
