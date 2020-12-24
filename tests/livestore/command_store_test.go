188884070b5b7d3908c2ff4c125188c4bcb53177
Brautigam Gergely <skarlso777@gmail.com>
2020-12-24 09:43:12 +0100

    Added failing test

--- tests/livestore/command_store_test.go
+++ tests/livestore/command_store_test.go
@@ -44,3 +44,44 @@ func TestCommandStore_Create(t *testing.T) {
 	require.NoError(t, err)
 	assert.Equal(t, 1, c.ID)
 }
+
+func TestCommandStore_Create_NameIsUnique(t *testing.T) {
+	logger := zerolog.New(os.Stderr)
+	location, _ := ioutil.TempDir("", "TestCommandStore_Create_NameIsUnique")
+	env := environment.NewDockerConverter(environment.Config{}, environment.Dependencies{Logger: logger})
+	cp := livestore.NewCommandStore(livestore.CommandDependencies{
+		Connector: livestore.NewDatabaseConnector(livestore.Config{
+			Hostname: dbaccess.Hostname,
+			Database: dbaccess.Db,
+			Username: dbaccess.Username,
+			Password: dbaccess.Password,
+		}, livestore.Dependencies{
+			Logger:    logger,
+			Converter: env,
+		}),
+	})
+	// Create the first command.
+	c, err := cp.Create(context.Background(), &models.Command{
+		Name:         "Test",
+		Schedule:     "test-schedule",
+		Repositories: nil,
+		Filename:     "test-filename",
+		Location:     location,
+		Hash:         "hash",
+		Enabled:      false,
+	})
+	require.NoError(t, err)
+	assert.Equal(t, 1, c.ID)
+
+	// Create the second command with the same name.
+	_, err = cp.Create(context.Background(), &models.Command{
+		Name:         "Test",
+		Schedule:     "test-schedule",
+		Repositories: nil,
+		Filename:     "test-filename",
+		Location:     location,
+		Hash:         "hash",
+		Enabled:      false,
+	})
+	require.Error(t, err)
+}
