49379b5140f551717e92d6d94d4fdcabf6a1bd02
Brautigam Gergely <skarlso777@gmail.com>
2021-01-09 14:20:54 +0100

    Added opts to the plugins and the hooks

--- pkg/krok/plugin.go
+++ pkg/krok/plugin.go
@@ -3,5 +3,8 @@ package krok
 // Plugin defines a Krok plugin and how it should behave and what it should be capable of.
 // This interface is returned by the plugin loader.
 type Plugin interface {
-	Execute(payload string) (string, bool, error)
+	// Execute defines the ability to run a command on a hook.
+	// Opts defines a variable number of arguments that can be passed to the command.
+	// These could come from the Vault, things like, auth information or environment properties, etc.
+	Execute(payload string, opts ...interface{}) (string, bool, error)
 }
