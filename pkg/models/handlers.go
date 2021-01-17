923e69bb60fdf915a3b864569d830196e9fc370e
Sam Perrin <samtperrin@gmail.com>
2021-01-17 10:49:51 +0000

    feat: remove unused VCS map

--- pkg/models/handlers.go
+++ pkg/models/handlers.go
@@ -12,11 +12,3 @@ const (
 	// BITBUCKET based hooks
 	BITBUCKET
 )
-
-// VCS is a map for mapping a VCS string value to int.
-var VCS = map[string]int{
-	"GITHUB":    1,
-	"GITLAB":    2,
-	"GITEA":     3,
-	"BITBUCKET": 4,
-}
