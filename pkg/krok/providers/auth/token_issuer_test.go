fec6157efc4914b4d25f60f90c7ff0c7c58c1722
Sam Perrin <samtperrin@gmail.com>
2021-02-13 11:15:54 +0000

    fix: Create util funcs for generating tokens for testd

--- pkg/krok/providers/auth/token_issuer_test.go
+++ pkg/krok/providers/auth/token_issuer_test.go
@@ -6,14 +6,38 @@ import (
 	"testing"
 	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
 	"golang.org/x/oauth2"
 
 	"github.com/krok-o/krok/pkg/krok/providers"
 	"github.com/krok-o/krok/pkg/krok/providers/mocks"
 	"github.com/krok-o/krok/pkg/models"
 )
+
+func generateValidToken(t *testing.T) string {
+	claims := jwt.StandardClaims{
+		Subject:   "1",
+		ExpiresAt: time.Now().Add(time.Second * 30).Unix(),
+		IssuedAt:  time.Now().Unix(),
+	}
+	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString([]byte("test"))
+	require.NoError(t, err)
+	return token
+}
+
+func generateExpiredToken(t *testing.T) string {
+	claims := jwt.StandardClaims{
+		Subject:   "1",
+		ExpiresAt: time.Now().Add(-time.Second * 30).Unix(),
+		IssuedAt:  time.Now().Unix(),
+	}
+	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString([]byte("test"))
+	require.NoError(t, err)
+	return token
+}
 
 func TestTokenIssuer_Create(t *testing.T) {
 	now, _ := time.Parse(time.RFC3339, "2020-01-31T15:00:00Z")
@@ -45,15 +69,9 @@ func TestTokenIssuer_Create(t *testing.T) {
 
 func TestTokenIssuer_Refresh(t *testing.T) {
 	now, _ := time.Parse(time.RFC3339, "2020-01-31T15:00:00Z")
-	expectedTokenResponse := &oauth2.Token{
-		TokenType:    "Bearer",
-		AccessToken:  `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1ODA0ODM3MDAsImlhdCI6MTU4MDQ4MjgwMCwic3ViIjoiMSJ9.apom8FiBl_QEfRYVkp-PDETLFzAdEFzVZLVMqrkj6Uc`,
-		RefreshToken: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1ODEwODc2MDAsImlhdCI6MTU4MDQ4MjgwMCwic3ViIjoiMSJ9.U3ocf3xQv8r5bzbr3l9IwAnCpqMDkOfdsNxkUktINSU`,
-		Expiry:       time.Unix(1580483700, 0),
-	}
 
 	t.Run("refresh expired token returns error", func(t *testing.T) {
-		expiredToken := `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1ODA0ODM3MDAsImlhdCI6MTU4MDQ4MjgwMCwic3ViIjoiMSJ9.apom8FiBl_QEfRYVkp-PDETLFzAdEFzVZLVMqrkj6Uc`
+		expiredToken := generateExpiredToken(t)
 
 		cfg := TokenIssuerConfig{
 			GlobalTokenKey: "test",
@@ -68,8 +86,7 @@ func TestTokenIssuer_Refresh(t *testing.T) {
 	})
 
 	t.Run("get user from store error", func(t *testing.T) {
-		// 50 year expiry
-		validToken := `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MTMyMDYxOTAsImlhdCI6MTYxMjYwMTM5MCwic3ViIjoiMSJ9.fWt4SleEugX_9kCc2aNQeCkkG0_4SZhww0Z9u3ASwe0`
+		validToken := generateValidToken(t)
 
 		mockUserStorer := &mocks.UserStorer{}
 		mockUserStorer.On("Get", mock.Anything, 1).Return(nil, errors.New("err"))
@@ -89,9 +106,8 @@ func TestTokenIssuer_Refresh(t *testing.T) {
 		mockUserStorer.AssertExpectations(t)
 	})
 
-	t.Run("get user from store error", func(t *testing.T) {
-		// 50 year expiry
-		validToken := `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MTMyMDYxOTAsImlhdCI6MTYxMjYwMTM5MCwic3ViIjoiMSJ9.fWt4SleEugX_9kCc2aNQeCkkG0_4SZhww0Z9u3ASwe0`
+	t.Run("get token success", func(t *testing.T) {
+		validToken := generateValidToken(t)
 
 		mockUserStorer := &mocks.UserStorer{}
 		mockUserStorer.On("Get", mock.Anything, 1).Return(&models.User{ID: 1}, nil)
@@ -112,6 +128,13 @@ func TestTokenIssuer_Refresh(t *testing.T) {
 		mockClock.AssertExpectations(t)
 		mockUserStorer.AssertExpectations(t)
 		assert.NoError(t, err)
+
+		expectedTokenResponse := &oauth2.Token{
+			TokenType:    "Bearer",
+			AccessToken:  `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1ODA0ODM3MDAsImlhdCI6MTU4MDQ4MjgwMCwic3ViIjoiMSJ9.apom8FiBl_QEfRYVkp-PDETLFzAdEFzVZLVMqrkj6Uc`,
+			RefreshToken: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1ODEwODc2MDAsImlhdCI6MTU4MDQ4MjgwMCwic3ViIjoiMSJ9.U3ocf3xQv8r5bzbr3l9IwAnCpqMDkOfdsNxkUktINSU`,
+			Expiry:       time.Unix(1580483700, 0),
+		}
 		assert.Equal(t, token, expectedTokenResponse)
 	})
 }
