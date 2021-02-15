751ebc00a60c4c45b78a43490ad592b1fc93c056
Gergely <gergely.brautigam@ibm.com>
2021-02-15 14:41:39 +0100

    Added a script to generate mock files

---
+++ generate_interface_mocks/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+	cwd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	currentFolder := filepath.Base(cwd)
+	files := make([]string, 0)
+	if err := filepath.Walk(cwd, func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() && info.Name() != currentFolder {
+			return filepath.SkipDir
+		}
+		if filepath.Ext(path) == ".go" {
+			files = append(files, info.Name())
+		}
+		return nil
+	}); err != nil {
+		log.Fatal(err)
+	}
+	for _, filename := range files {
+		fset := token.NewFileSet()
+		node, err := parser.ParseFile(fset, filename, nil, parser.ParseComments)
+		if err != nil {
+			fmt.Println("Error parsing file: " + filename)
+			os.Exit(255)
+		}
+
+		base := strings.TrimSuffix(filename, filepath.Ext(filename))
+		ast.Inspect(node, func(n ast.Node) bool {
+			switch t := n.(type) {
+			case *ast.TypeSpec:
+				if t.Name.IsExported() {
+					switch t.Type.(type) {
+					case *ast.InterfaceType:
+						out, err := exec.Command("mockery", "--name", t.Name.Name, "--filename", fmt.Sprintf("%s_%s.go", base, t.Name.Name)).Output()
+						if err != nil {
+							log.Fatal(err)
+						}
+						log.Println(out)
+					}
+				}
+			}
+			return true
+		})
+	}
+}
