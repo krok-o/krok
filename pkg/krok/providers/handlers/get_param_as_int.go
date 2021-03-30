5ad04aa49008b7fcb152567e6484d9db5f746648
skarlso <skarlso777@gmail.com>
2021-03-30 15:02:43 +0200

    Added missing file

---
+++ pkg/krok/providers/handlers/get_param_as_int.go
@@ -0,0 +1,23 @@
+package handlers
+
+import (
+	"errors"
+	"strconv"
+
+	"github.com/labstack/echo/v4"
+)
+
+// GetParamAsInt returns a number for an echo context parameter
+// and checks its existance.
+func GetParamAsInt(name string, c echo.Context) (int, error) {
+	param := c.Param(name)
+	if param == "" {
+		return 0, errors.New("parameter not found")
+	}
+
+	n, err := strconv.Atoi(param)
+	if err != nil {
+		return 0, err
+	}
+	return n, nil
+}
