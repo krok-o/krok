332caef7f4a1a5339df44284854cb4b32db1446b
Gergely Brautigam <skarlso777@gmail.com>
2021-06-04 20:10:07 +0200

    Updating mocks and removed unused function

--- pkg/converter/string.go
+++ pkg/converter/string.go
@@ -1,13 +1,5 @@
 package converter
 
-// ToValue takes a pointer to a string value and returns the value.
-func ToValue(v *string) string {
-	if v == nil {
-		return ""
-	}
-	return *v
-}
-
 // ToPointer takes a value and creates a pointer out from it.
 func ToPointer(v string) *string {
 	return &v
