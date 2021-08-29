73bec7d905f0061b0955c6158471a93b29ec2cc9
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2021-08-29 18:05:39 +0200

    Added some coverage to this thing

--- pkg/krok/providers/executor/in_memory_executor_test.go
+++ pkg/krok/providers/executor/in_memory_executor_test.go
@@ -15,7 +15,7 @@ import (
 	"github.com/krok-o/krok/pkg/models"
 )
 
-func TestCreateRun(t *testing.T) {
+func TestInMemoryExecutor_CreateRun(t *testing.T) {
 	if _, err := client.NewClientWithOpts(client.FromEnv); err != nil {
 		t.Skip("Could not run test. This test requires Docker to be accessible.")
 	}
@@ -35,6 +35,7 @@ func TestCreateRun(t *testing.T) {
 		Outcome:     "",
 		CreateAt:    time.Date(1981, 1, 1, 1, 1, 1, 1, time.UTC),
 	}, nil)
+	mcr.On("UpdateRunStatus", mock.Anything, 1, "success", "\"platform: github,event-type: push,payload: e30=\"").Return(nil)
 	mcs := &mocks.CommandStorer{}
 	mcs.On("IsPlatformSupported", mock.Anything, 1, 1).Return(true, nil)
 	mcs.On("ListSettings", mock.Anything, 1).Return(nil, nil)
@@ -69,7 +70,7 @@ func TestCreateRun(t *testing.T) {
 					Auth: &models.Auth{Secret: "secret"},
 				},
 			},
-			Image:   "skarlso/slack-notification:v0.0.5",
+			Image:   "krokhook/integration-test-command:v0.0.1",
 			Enabled: true,
 			Platforms: []models.Platform{
 				{
@@ -80,6 +81,51 @@ func TestCreateRun(t *testing.T) {
 		},
 	})
 	assert.NoError(t, err)
-	// todo: wait here for the finishing of containers and such.
-	// also add a Skip if there is no docker socket available.
+	assert.NotEmpty(t, ime.runs)
+	assert.Eventually(t, func() bool {
+		var b bool
+		ime.runsLock.Lock()
+		b = len(ime.runs[1]) == 0
+		ime.runsLock.Unlock()
+		return b
+	}, 20*time.Second, 5*time.Second)
+}
+
+func TestInMemoryExecutor_CancelRun_NonExistent(t *testing.T) {
+	logger := zerolog.New(os.Stderr)
+	mcr := &mocks.CommandRunStorer{}
+	mcs := &mocks.CommandStorer{}
+	mt := &mocks.Clock{}
+	ime := NewInMemoryExecutor(Config{
+		DefaultMaximumCommandRuntime: 10,
+	}, Dependencies{
+		Logger:        logger,
+		CommandRuns:   mcr,
+		CommandStorer: mcs,
+		Clock:         mt,
+	})
+	err := ime.CancelRun(context.Background(), 88)
+	assert.EqualError(t, err, "run with ID not found")
+}
+
+func TestInMemoryExecutor_CancelRun_WithNoCommandsDeletesEventEntry(t *testing.T) {
+	logger := zerolog.New(os.Stderr)
+	mcr := &mocks.CommandRunStorer{}
+	mcs := &mocks.CommandStorer{}
+	mt := &mocks.Clock{}
+	ime := NewInMemoryExecutor(Config{
+		DefaultMaximumCommandRuntime: 10,
+	}, Dependencies{
+		Logger:        logger,
+		CommandRuns:   mcr,
+		CommandStorer: mcs,
+		Clock:         mt,
+	})
+	ime.runs[99] = make(map[string]string)
+	err := ime.CancelRun(context.Background(), 99)
+	assert.NoError(t, err)
+	ime.runsLock.Lock()
+	_, ok := ime.runs[99]
+	assert.False(t, ok)
+	ime.runsLock.Unlock()
 }
