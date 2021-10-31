45257250d4ea046ba3665997d058b7d38d920710
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2021-10-31 21:20:47 +0100

    Sadly this will not fly

--- pkg/server/server_grpc.go
+++ pkg/server/server_grpc.go
@@ -1,11 +1,27 @@
 package server
 
-import "context"
+import (
+	"context"
+	"fmt"
+	"net"
+
+	grpc_prometheus "github.com/grpc-ecosystem/go-grpc-prometheus"
+	"golang.org/x/sync/errgroup"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	"github.com/krok-o/krok/pkg/protos"
+)
 
 // GRPCKrokServer is a server.
 type GRPCKrokServer struct {
 	Config
 	Dependencies
+}
+
+// HandleHooks handles hooks.
+func (s *GRPCKrokServer) HandleHooks(ctx context.Context, request *protos.HandleHooksRequest) (*protos.HandleHooksResponse, error) {
+	panic("implement me")
 }
 
 // NewGRPCKrokServer creates a new GRPC krok server.
@@ -15,5 +31,26 @@ func NewGRPCKrokServer(cfg Config, deps Dependencies) *GRPCKrokServer {
 
 // Run starts up listening.
 func (s *GRPCKrokServer) Run(ctx context.Context) error {
-	return nil
+	// setup grpc server details
+	grpcLis, err := net.Listen("tcp:", ":50051")
+	if err != nil {
+		return fmt.Errorf("failed to listen on address %s: %v", ":50051", err)
+	}
+	grpcSrv := grpc.NewServer(
+		grpc.StreamInterceptor(grpc_prometheus.StreamServerInterceptor),
+		grpc.UnaryInterceptor(grpc_prometheus.UnaryServerInterceptor),
+	)
+	reflection.Register(grpcSrv)
+
+	// setup krok server details
+	protos.RegisterKrokServiceServer(grpcSrv, &GRPCKrokServer{})
+	g, _ := errgroup.WithContext(ctx)
+	g.Go(func() error {
+		if err := grpcSrv.Serve(grpcLis); err != nil {
+			s.Logger.Error().Err(err).Msg("unable to start grpc server")
+			return err
+		}
+		return nil
+	})
+	return g.Wait()
 }
